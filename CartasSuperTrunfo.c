03f98e8a2e5eb710d70c57d8ad1626f899d946fc
LucasR-creator <lucasrochadacruz0@gmail.com>
2025-02-19 12:46:48 +0000

    demonstração do kithub

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+int main () {
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+    int cidade;
+    char estado;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Entrada da letra do estado
+    printf ("identifique qual estado de (A-H)\n");
+    scanf ("%c", &estado);
 
-    return 0;
+    // Entrada do número da cidade (A01, A02, B01, B02)
+    printf ("identifique numero da cidade (1-4)\n");
+    scanf ("%d", &cidade);
+
+    // Entrada da população
+    printf ("identifique numero da populacao\n");
+    scanf  ("%d", &populacao);
+
+    // Entrada da área
+    // Entrada da área
+    printf ("identifique numero da area\n");
+    scanf  ("%f", &area);
+
+    // Entrada do PIB
+    printf ("identifique numero do pib\n");
+    scanf  ("%f", &pib);
+
+     // Entrada dos pontos turísticos
+    printf ("identifique numero dos pontos pontos_turisticos\n");
+    scanf ("%d", &pontos_turisticos);
+
+// Exibir os dados inseridos
+printf("\nDados inseridos:\n");
+printf("Estado: %d\n", estado);
+printf("Cidade: %d\n", cidade);
+printf("Populacao: %d\n", populacao);
+printf("Area: %.2f km²\n", area);
+printf("PIB: %.2f\n", pib);
+printf("Pontos Turisticos: %d\n", pontos_turisticos);
+
+return 0;
 }
